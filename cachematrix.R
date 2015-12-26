beb75dc4177402cefa71019f89d401d9d62366d8
fernando <fernando.hillermann@gmail.com>
2015-12-26 10:11:59 -0600

    Saturdaty 26/12/15, 10:12am.
    This is the first commit of the assignment, function working properly.
    
    The function works fine. In this commit I included the "trycatch" command
    to see if I was able to handle errors due to a matrix that R is not able
    to solve (not a problem with my functions, but with "solve").

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,63 @@
-## Put comments here that give an overall description of what your
-## functions do
+# This functions allows you to calculate the inverse of a matrix.
+# If the calculation has already been done, then the result is
+# of the second function is going to be the first operation, 
+# that was stored in the cache.
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+cacheMatrix <- function(firstMatrix = matrix()) {
 
+    solvedM <- NULL
+    
+    setValues <- function(x){
+        
+        solvedM <<- NULL
+        firstMatrix <<- x
+        
+    }
+    
+    storeMatrix <- function(x){
+        
+        solvedM <<- x
+        
+    }
+    
+    obtainCalculation <- function() solvedM
+    
+    originalMatrix <- function() firstMatrix
+    
+    list(Store = storeMatrix, Obtain = obtainCalculation, Original = originalMatrix, Set = setValues)
+    
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+matrixSolve <- function(par1, ...) {
         ## Return a matrix that is the inverse of 'x'
+    
+    checkStatus <- par1$Obtain()
+    
+    if (!is.null(checkStatus)){
+        
+        print("The matrix inverse has already been calculated. Getting cached data from 'Obtain calculation' function.")
+        solvedMatrix <- par1$Obtain()
+        return(solvedMatrix)
+        
+    }
+    
+    original <- par1$Original()
+    tryCatch ({
+        
+        print("The matrix inverse hasn't been calculated. Doing calculation now...");
+        original <- solve(original); 
+        par1$Store(original); 
+        return(original) }, 
+        error = function(e){
+            
+            "Please check the matrix you are trying to solve"
+            
+            }
+    )
+    
 }
